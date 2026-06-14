938d2ef27d1a18631987bbb2d860f9a3db7233c4
Elena Charlsworth <elenadoweb@gmail.com>
2026-06-14 20:31:56 +0300

    fix: новые фото jpg3, close-секция отодвинута от баннера, типографика

--- upload.ps1
+++ upload.ps1
@@ -5,15 +5,15 @@ $wc.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
 $b = "ftp://ftp.magcard.ru/www/webhellen.com/jpg3"
 $src = "C:\Users\lena\Desktop\Start\jpg3"
 
-$wc.UploadFile("$b/1.jpg", "$src\_1.jpg")
-Write-Host "1.jpg"
-$wc.UploadFile("$b/2.jpg", "$src\_2.avif")
-Write-Host "2.jpg"
-$wc.UploadFile("$b/3.jpg", "$src\_3.jpg")
-Write-Host "3.jpg"
-$wc.UploadFile("$b/4.jpg", "$src\_4.jpg")
-Write-Host "4.jpg"
-$wc.UploadFile("$b/5.gif", "$src\0525.gif")
-Write-Host "5.gif"
+# Upload all 17
+for ($i=1; $i -le 17; $i++) {
+    $files = Get-ChildItem "$src\_$i.*"
+    if ($files) {
+        $ext = $files.Extension
+        $wc.UploadFile("$b/$i$ext", $files.FullName)
+        Write-Host "$i$ext"
+    }
+}
+
 $wc.UploadFile("ftp://ftp.magcard.ru/www/webhellen.com/kp.html", "C:\Users\lena\Desktop\Start\dresslab-kp.html")
 Write-Host "HTML DONE"
