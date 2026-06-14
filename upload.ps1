0827bac22aa3a18f9005e421bdb2a89aa16c38f1
Elena Charlsworth <elenadoweb@gmail.com>
2026-06-14 20:18:40 +0300

    feat: обновлены фото в jpg3 на новые

--- upload.ps1
+++ upload.ps1
@@ -1,21 +1,19 @@
 $user = "u3458917"
 $pass = "Constantine01?!"
-
-# Create jpg3 dir on server
-$req = [System.Net.FtpWebRequest]::Create("ftp://ftp.magcard.ru/www/webhellen.com/jpg3")
-$req.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
-$req.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
-try { $req.GetResponse().Close() } catch { }
-
 $wc = New-Object System.Net.WebClient
 $wc.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
 $b = "ftp://ftp.magcard.ru/www/webhellen.com/jpg3"
 $src = "C:\Users\lena\Desktop\Start\jpg3"
 
 $wc.UploadFile("$b/1.jpg", "$src\_1.jpg")
-$wc.UploadFile("$b/2.jpg", "$src\86a4b3b8-0647-4853-9b10-037324696866.png")
-$wc.UploadFile("$b/3.jpg", "$src\932c3bf585911f1a27c4a5fe05787c5_1.jpeg")
-$wc.UploadFile("$b/4.jpg", "$src\fashion-african-woman-red-dress-600nw-2147305089.webp")
+Write-Host "1.jpg"
+$wc.UploadFile("$b/2.jpg", "$src\_2.avif")
+Write-Host "2.jpg"
+$wc.UploadFile("$b/3.jpg", "$src\_3.jpg")
+Write-Host "3.jpg"
+$wc.UploadFile("$b/4.jpg", "$src\_4.jpg")
+Write-Host "4.jpg"
 $wc.UploadFile("$b/5.gif", "$src\0525.gif")
+Write-Host "5.gif"
 $wc.UploadFile("ftp://ftp.magcard.ru/www/webhellen.com/kp.html", "C:\Users\lena\Desktop\Start\dresslab-kp.html")
-Write-Host "DONE"
+Write-Host "HTML DONE"
