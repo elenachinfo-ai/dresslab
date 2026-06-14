57dca07e8b600922f1c3b6b7080a76bf97a0e129
Elena Charlsworth <elenadoweb@gmail.com>
2026-06-14 20:07:32 +0300

    feat: свои фото из jpg3 вместо Unsplash во всех блоках КП

--- upload.ps1
+++ upload.ps1
@@ -1,28 +1,21 @@
-$user = 'u3458917'
-$pass = 'Constantine01?!'
-$base = 'ftp://ftp.magcard.ru'
+$user = "u3458917"
+$pass = "Constantine01?!"
+
+# Create jpg3 dir on server
+$req = [System.Net.FtpWebRequest]::Create("ftp://ftp.magcard.ru/www/webhellen.com/jpg3")
+$req.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
+$req.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
+try { $req.GetResponse().Close() } catch { }
+
 $wc = New-Object System.Net.WebClient
 $wc.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
+$b = "ftp://ftp.magcard.ru/www/webhellen.com/jpg3"
+$src = "C:\Users\lena\Desktop\Start\jpg3"
 
-# Upload to webhellen.com folder
-$wc.UploadFile("$base/www/webhellen.com/kp.html", 'C:\Users\lena\Desktop\Start\dresslab-kp.html')
-Write-Host 'kp.html -> webhellen.com/ ok'
-
-# Create subdirs
-$d1 = [System.Net.FtpWebRequest]::Create("$base/www/webhellen.com/ipg")
-$d1.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
-$d1.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
-try { $d1.GetResponse().Close() } catch {}
-$d2 = [System.Net.FtpWebRequest]::Create("$base/www/webhellen.com/jpg2")
-$d2.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
-$d2.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
-try { $d2.GetResponse().Close() } catch {}
-
-# Upload media
-$wc.UploadFile("$base/www/webhellen.com/ipg/photo1.jpg", 'C:\Users\lena\Desktop\Start\ipg\photo_2026-06-14_19-15-58.jpg')
-$wc.UploadFile("$base/www/webhellen.com/ipg/photo2.jpg", 'C:\Users\lena\Desktop\Start\ipg\photo_2026-06-14_19-15-58 (2).jpg')
-$wc.UploadFile("$base/www/webhellen.com/ipg/video.mp4", 'C:\Users\lena\Desktop\Start\ipg\video_2026-06-14_19-20-56.mp4')
-$wc.UploadFile("$base/www/webhellen.com/jpg2/hero.mp4", 'C:\Users\lena\Desktop\Start\jpg2\video_2026-06-14_19-30-08.mp4')
-Write-Host 'Media uploaded!'
-
-Write-Host 'DONE: https://webhellen.com/kp.html'
+$wc.UploadFile("$b/1.jpg", "$src\_1.jpg")
+$wc.UploadFile("$b/2.jpg", "$src\86a4b3b8-0647-4853-9b10-037324696866.png")
+$wc.UploadFile("$b/3.jpg", "$src\932c3bf585911f1a27c4a5fe05787c5_1.jpeg")
+$wc.UploadFile("$b/4.jpg", "$src\fashion-african-woman-red-dress-600nw-2147305089.webp")
+$wc.UploadFile("$b/5.gif", "$src\0525.gif")
+$wc.UploadFile("ftp://ftp.magcard.ru/www/webhellen.com/kp.html", "C:\Users\lena\Desktop\Start\dresslab-kp.html")
+Write-Host "DONE"
