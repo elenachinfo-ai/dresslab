1b25763bded9ba72b3c906cd3d8152c0e5003816
Elena Charlsworth <elenadoweb@gmail.com>
2026-06-14 21:00:40 +0300

    fix: видео — убран overlay-постер, нативный poster, файл перезалит

--- u.ps1
+++ u.ps1
@@ -1,12 +1,6 @@
-$user="u3458917"
-$pass="Constantine01?!"
 $wc=New-Object System.Net.WebClient
-$wc.Credentials=New-Object System.Net.NetworkCredential($user,$pass)
-$b="ftp://ftp.magcard.ru/www/webhellen.com/jpg3"
-$s="C:\Users\lena\Desktop\Start\jpg3"
-for($i=1;$i -le 16;$i++){
-  $f=Get-ChildItem "$s\_$i.*"
-  if($f){$wc.UploadFile("$b/$i$($f.Extension)",$f.FullName);Write-Host "$i$($f.Extension)"}
-}
-$wc.UploadFile("ftp://ftp.magcard.ru/www/webhellen.com/kp.html","C:\Users\lena\Desktop\Start\dresslab-kp.html")
-Write-Host "DONE"
+$wc.Credentials=New-Object System.Net.NetworkCredential('u3458917','Constantine01?!')
+$wc.UploadFile('ftp://ftp.magcard.ru/www/webhellen.com/ipg/video.mp4','C:\Users\lena\Desktop\Start\ipg\video_2026-06-14_19-20-56.mp4')
+Write-Host 'video re-uploaded'
+$wc.UploadFile('ftp://ftp.magcard.ru/www/webhellen.com/kp.html','C:\Users\lena\Desktop\Start\dresslab-kp.html')
+Write-Host 'HTML updated'
