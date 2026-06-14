2dc50bfb04d62d731392c2fdf64311079bf25ee8
Elena Charlsworth <elenadoweb@gmail.com>
2026-06-14 20:40:25 +0300

    redesign: премиум фэшн-журнальный стиль, full-width фото, подписи, подвал, чистые карточки

---
+++ u.ps1
@@ -0,0 +1,4 @@
+$wc=New-Object System.Net.WebClient
+$wc.Credentials=New-Object System.Net.NetworkCredential('u3458917','Constantine01?!')
+$wc.UploadFile('ftp://ftp.magcard.ru/www/webhellen.com/kp.html','C:\Users\lena\Desktop\Start\dresslab-kp.html')
+Write-Host 'OK'
